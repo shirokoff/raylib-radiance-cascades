ab6ba9d1c0a6a84e372ce50d3574f366a922d843
Ivan Shirokov <shirokoff@ya.ru>
2025-11-09 14:58:36 +0000

    Working texture

--- radiance.fs.glsl
+++ radiance.fs.glsl
@@ -6,7 +6,5 @@ out vec4 finalColor;
 
 void main()
 {
-    // simple debug fragment shader: output the input texture directly
-    vec3 col = texture(uCells, fragTexCoord).rgb;
-    finalColor = vec4(col, 1.0);
+    finalColor = texture(uCells, fragTexCoord);   
 }
